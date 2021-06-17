f233c235951d481cb5549d46f9539efc724383f4
Aleksandr <aswom@mail.ru>
2021-06-17 19:34:59 +0200

    evening17

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-$(function(){
+$(document).ready(function(){
    $(".slides").slick({
-       infinite: true,
-  slidesToShow: 2,
+      infinite: true,
+      slidesToShow: 2,
       slidesToScroll: 1,
       dots: true,
-   arrows: true
+      arrows: true,
+      responsive:[
+{
+    breakpoint: 768,
+    settings: {
+        slidesToShow:2
+    }
+},
+{
+    breakpoint: 550,
+    settings: {
+        slidesToShow:1
+    }
+}
+]
+      // centerMode: true,
+      //centerPadding:'0px',
+     // prevArrow: "<img src='/img/left-arrow-svgrepo-com.svg' width='20%' class='prev' alt='1'> ",
+     // nextArrow:"<img src='/img/right-arrow-svgrepo-com.svg'  width='20%' class='next' alt='2'> "
    })
 })
